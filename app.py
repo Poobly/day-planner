291e0c8e47d7880318bfd79e8637766b1110f3c7
Poobly <79822237+Poobly@users.noreply.github.com>
2023-12-25 19:19:51 -0500

    website structure and some html additions

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, redirect, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
-
+from helpers import login_required
 
 app = Flask(__name__)
 Session(app)
+
 
 @app.route("/")
 def index():
@@ -33,3 +34,39 @@ def profile():
         return redirect("/")
     else:
         return render_template("profile.html")
+
+@app.route("/planner", methods=["GET", "POST"])
+def planner():
+    if request.method == "POST":
+        return redirect("/")
+    else:
+        return render_template("planner/planner.html")
+
+@app.route("/planner/calendar", methods=["GET", "POST"])
+def planner_calendar():
+    if request.method == "POST":
+        return redirect("/")
+    else:
+        return render_template("planner/calendar.html")
+    
+@app.route("/planner/edit", methods=["GET", "POST"])
+def planner_edit():
+    if request.method == "POST":
+        return redirect("/")
+    else:
+        return render_template("planner/edit.html")
+
+@app.route("/pomodoro", methods=["GET", "POST"])
+def pomodoro():
+    if request.method == "POST":
+        return redirect("/")
+    else:
+        return render_template("pomodoro/pomodoro.html")
+        
+@app.route("/pomodoro/progress", methods=["GET", "POST"])
+def pomodoro_progress():
+    if request.method == "POST":
+        return redirect("/")
+    else:
+        return render_template("pomodoro/progress.html")
+
