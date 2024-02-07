07e697a72c393ea26af023685a63215c6e1bbc3c
Poobly <79822237+Poobly@users.noreply.github.com>
2024-02-06 23:25:27 -0500

    created javascript and css file, also code to get all days of the year for the calendar

--- static/scripts/calendar.js
+++ static/scripts/calendar.js
@@ -2,3 +2,15 @@
 // Create calendar
 // Display events from backend or from local storage
 // 
+
+const current_date = new Date;
+const year = current_date.getFullYear(); 
+
+let date = new Date(year, 0, 1);
+
+while (date.getFullYear() === year) {
+
+
+    console.log(date);
+    date.setDate(date.getDate() + 1);
+}
