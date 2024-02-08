29b0cd1d629829b85d2d46b6c07846ae95bccf21
Poobly <79822237+Poobly@users.noreply.github.com>
2024-02-08 17:22:40 -0500

    created obj with the table row elements for each week

--- static/scripts/calendar.js
+++ static/scripts/calendar.js
@@ -6,6 +6,17 @@
 const current_date = new Date;
 const year = current_date.getFullYear(); 
 
+const week_rows = document.getElementsByClassName("table-week-row");
+
+const weeks_obj = {
+    0 : week_rows[0],
+    1 : week_rows[1],
+    2 : week_rows[2],
+    3 : week_rows[3],
+    4 : week_rows[4],
+    5 : week_rows[5],
+}
+
 let date = new Date(year, 0, 1);
 
 while (date.getFullYear() === year) {
