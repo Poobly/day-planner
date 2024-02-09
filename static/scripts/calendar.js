ae1649add71d374145b6cf3729f8c97588124798
Poobly <79822237+Poobly@users.noreply.github.com>
2024-02-08 21:49:54 -0500

    added unfunctional previous/next month buttons, added class and dataset to elements for each day

--- static/scripts/calendar.js
+++ static/scripts/calendar.js
@@ -4,7 +4,7 @@
 
 const current_date = new Date;
 const year = current_date.getFullYear();
-const month = current_date.getMonth();  
+let month = current_date.getMonth();  
 
 
 const week_rows = document.getElementsByClassName("table-week-row");
@@ -15,13 +15,28 @@ const weeks_obj = {
     2 : week_rows[2],
     3 : week_rows[3],
     4 : week_rows[4],
-    5 : week_rows[5],
+    5 : week_rows[5]
 }
 
 let date = new Date(year, month, 1);
 let new_date = new Date(year, month, 1);
 
 let weeks = 0;
+
+const next_month_button = document.getElementById("calendar-asc-button");
+const previous_month_button = document.getElementById("calendar-desc-button");
+
+previous_month_button.addEventListener("click", (e) => {
+    month--;
+    loadCalendar();
+})
+
+next_month_button.addEventListener("click", (e) => {
+    month++;
+    loadCalendar();
+})
+
+
 
 
 while (date.getFullYear() === year && date.getMonth() === month) {
@@ -40,17 +55,24 @@ while (date.getFullYear() === year && date.getMonth() === month) {
      */
     if (date.getDate() === 1 && date.getDay() !== 0 ) {
         for (let i = date.getDay(); i > 0; i--) {
-            const last_month_element = document.createElement("td");
+            const last_month_element_con = document.createElement("td");
             new_date.setDate(date.getDate() - i);
-            last_month_element.textContent = new_date.getDate();
-            weeks_obj[weeks].appendChild(last_month_element);
+            last_month_element_con.textContent = new_date.getDate();
+            last_month_element_con.classList.add("day-con");
+            last_month_element_con.dataset.date = new_date.toLocaleString(undefined, {year: "numeric", month: "2-digit", day: "2-digit"});
+            weeks_obj[weeks].appendChild(last_month_element_con);
             new_date.setDate(new_date.getDate() + i);
         }
     }
 
     const day_element_con = document.createElement("td");
     day_element_con.textContent = date.getDate();
+    day_element_con.classList.add("day-con");
+    day_element_con.dataset.date = date.toLocaleString(undefined, {year: "numeric", month: "2-digit", day: "2-digit"});
+
     weeks_obj[weeks].appendChild(day_element_con);
+
+
     date.setDate(date.getDate() + 1);
 }
 
@@ -62,9 +84,13 @@ if (date.getMonth() !== month) {
     for (let i = weeks; i < 6; i++) {
         for (let j = new_date.getDay(); j < 7; j++) {
 
-            const new_month_element  = document.createElement("td");
-            new_month_element.textContent = new_date.getDate();
-            weeks_obj[i].appendChild(new_month_element);
+            const new_month_element_con  = document.createElement("td");
+            new_month_element_con.textContent = new_date.getDate();
+            new_month_element_con.classList.add("day-con");
+            new_month_element_con.dataset.date = new_date.toLocaleString(undefined, {year: "numeric", month: "2-digit", day: "2-digit"});
+            
+            
+            weeks_obj[i].appendChild(new_month_element_con);
 
             new_date.setDate(new_date.getDate() + 1); 
 
