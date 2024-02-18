606dbb45129a6fa718691c18515f4146891b53b6
Poobly <79822237+Poobly@users.noreply.github.com>
2024-02-18 16:59:58 -0500

    fixed the progress calendar's session progress display on days apart from current day

--- static/scripts/progress.js
+++ static/scripts/progress.js
@@ -1,10 +1,14 @@
 // WIP: 
 // if user is logged in get data from backend else use local storage
 
+import { toIsoStringLocale } from "./helpers.js"
+
 const progress_table = document.getElementById("progress-table");
 const day_rows = progress_table.querySelectorAll("tbody > tr");
 const current_date = new Date; 
 let year = current_date.getFullYear();
+
+const sessions = JSON.parse(localStorage.getItem("elapsed_sessions")); 
 
 let total_days = (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) ? 366 : 365;
 
@@ -33,16 +37,22 @@ const months_object = {
     11 : document.getElementById("header-dec")
 };
 
-let active_element = false;
 
 const days = {}
-const today = new Date().toLocaleString(undefined, {year: "numeric", month: "2-digit", day: "2-digit"});
+const today = toIsoStringLocale(new Date()).slice(0, 10);
 let date = new Date(year, 0, 1);
 
 let current_month = date.getMonth();
 let next_month = date.getMonth() + 1;
 let colspan_count = 0;
 
+
+function date_object(sessions, element) {
+    return {
+        sessions, 
+        element
+    };
+}
 
 while (date.getFullYear() === year) {
     let current_day = date.getDay();
@@ -66,8 +76,7 @@ while (date.getFullYear() === year) {
 
     months_object[current_month].colSpan = colspan_count;
 
-    let new_date = date.toLocaleString(undefined, {year: "numeric", month: "2-digit", day: "2-digit"});
-    days[new_date] = 0;
+    let new_date = toIsoStringLocale(date).slice(0, 10);
     date.setDate(date.getDate() + 1);
     current_month = date.getMonth();
 
@@ -75,16 +84,21 @@ while (date.getFullYear() === year) {
     day.classList.add("progress-tile");
     day.dataset.date = new_date;
     
+    days[new_date] = date_object(0, day);
     days_object[current_day].appendChild(day);
     
     // check if user logged in and get data from db and insert into days object e.g. days[new_date] = database_data[new_date] 
+
     if (today == new_date) {
-        days[new_date] = JSON.parse(localStorage.getItem("elapsed_sessions")); 
-        
-        if (days[new_date] > 5) day.classList.add("progress-tile-4");
-        else if (days[new_date] > 4) day.classList.add("progress-tile-3");
-        else if (days[new_date] > 2) day.classList.add("progress-tile-2");
-        else if (days[new_date] > 0) day.classList.add("progress-tile-1");
+        for (let session in sessions) {
+            days[session].sessions = sessions[session]
+
+            if (days[session].sessions > 5) days[session].element.classList.add("progress-tile-4");
+            else if (days[session].sessions > 4) days[session].element.classList.add("progress-tile-3");
+            else if (days[session].sessions > 2) days[session].element.classList.add("progress-tile-2");
+            else if (days[session].sessions > 0) days[session].element.classList.add("progress-tile-1");
+        }
+    
     }
 
 
@@ -93,10 +107,9 @@ while (date.getFullYear() === year) {
         day.appendChild(tool_tip);
         tool_tip.classList.add("tool-tip");
 
-        if (days[new_date] > 0) tool_tip.textContent = `You have done ${days[new_date]} work sessions.`; 
+        if (days[new_date].sessions > 0) tool_tip.textContent = `You have done ${days[new_date].sessions} work sessions.`; 
         else tool_tip.textContent = `You have done no work sessions.`; 
 
-        active_element = day;
     });
 
     day.addEventListener("mouseleave", (e) => {
