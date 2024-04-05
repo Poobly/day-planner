bd8d4fc061136738b3aaf58d0559fac3756b3e24
Poobly <79822237+Poobly@users.noreply.github.com>
2024-04-05 14:23:13 -0400

    changed sql structure

--- planner.sql
+++ planner.sql
@@ -1,4 +1,4 @@
-CREATE TABLE plans (
+CREATE TABLE plan_details (
     id INTEGER,
     time TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL,
     PRIMARY KEY(id)
@@ -11,9 +11,37 @@ CREATE TABLE users (
     PRIMARY KEY(id)
 );
 
-CREATE TABLE assignment_table (
+CREATE TABLE plans (
     user_id INTEGER NOT NULL,
     plan_id INTEGER NOT NULL,
     FOREIGN KEY(user_id) REFERENCES users(id),
-    FOREIGN KEY(plan_id) REFERENCES plans(id)
+    FOREIGN KEY(plan_id) REFERENCES plan_details(id)
 );
+
+CREATE TABLE pomodoros (
+    user_id INTEGER NOT NULL,
+    pomodoro_id INTEGER NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(pomodoro_id) REFERENCES pomodoro_details(id)
+);
+
+CREATE TABLE pomodoro_details (
+    id INTEGER,
+    date TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    count INTEGER NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE settings (
+    id INTEGER,
+    description TEXT,
+    PRIMARY KEY(id)
+)
+
+CREATE TABLE user_settings (
+    user_id INTEGER NOT NULL,
+    settings_id INTEGER NOT NULL,
+    value BOOLEAN NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(settings_id) REFERENCES settings(id)
+)
